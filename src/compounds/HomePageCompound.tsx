107a6ca0764fc3a1cf8bfd364822789897422342
Jomana ahmed <ojy.ahmed@outlook.com>
2023-08-26 14:13:03 +0200

    Adding Navbar.tsx file to the HomePageCompound.tsx file

--- src/compounds/HomePageCompound.tsx
+++ src/compounds/HomePageCompound.tsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import styled from "styled-components"
+import React from "react";
+import styled from "styled-components";
+import Navbar from "../components/HomePage/Navbar";
 
 function HomePageCompound() {
   return (
     <Wrapper>
-       
+      <Navbar />
     </Wrapper>
   );
 }
 
-export const Wrapper = styled.div`
-  
-`
+export const Wrapper = styled.div``;
 
 export default HomePageCompound;
