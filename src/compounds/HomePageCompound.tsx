199acc85f586e5a92a881af88f8da3803633ce38
Jomana ahmed <ojy.ahmed@outlook.com>
2023-08-27 13:03:36 +0200

    Adding Volunteer.tsx file to the HomePageCompound.tsx file

--- src/compounds/HomePageCompound.tsx
+++ src/compounds/HomePageCompound.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import styled from "styled-components";
 import Navbar from "../components/HomePage/Navbar";
 import Banner from "../components/HomePage/Banner";
+import Volunteer from "../components/HomePage/Volunteer";
 
 function HomePageCompound() {
   return (
     <Wrapper>
       <Navbar />
       <Banner />
+      <Volunteer />
     </Wrapper>
   );
 }
