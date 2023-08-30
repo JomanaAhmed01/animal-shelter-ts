ed6434b78d9b0352eb43a7da71f53f5fa56058f0
Jomana ahmed <ojy.ahmed@outlook.com>
2023-08-30 14:12:47 +0200

    Creating Events.tsx file

---
+++ src/components/HomePage/Events.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+function Events() {
+  return (
+    <Wrapper>
+      
+    </Wrapper>
+  );
+}
+
+export const Wrapper = styled.div`
+  
+`;
+
+export default Events;
