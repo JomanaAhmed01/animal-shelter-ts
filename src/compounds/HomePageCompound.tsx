b4438a3f8c7d3da898cbc24a0baab7249ee57eef
Jomana ahmed <ojy.ahmed@outlook.com>
2023-08-30 12:47:52 +0200

    Adding AdoptableAnimals.tsx file to the HomePageCompound.tsx file

--- src/compounds/HomePageCompound.tsx
+++ src/compounds/HomePageCompound.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import Navbar from "../components/HomePage/Navbar";
 import Banner from "../components/HomePage/Banner";
 import Volunteer from "../components/HomePage/Volunteer";
+import AdoptableAnimals from "../components/HomePage/AdoptableAnimals";
 
 function HomePageCompound() {
   return (
@@ -10,6 +11,7 @@ function HomePageCompound() {
       <Navbar />
       <Banner />
       <Volunteer />
+      <AdoptableAnimals />
     </Wrapper>
   );
 }
