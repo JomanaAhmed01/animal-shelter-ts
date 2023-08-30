af07f6fe8dc992ad5550e66e164561589ebe86af
Jomana ahmed <ojy.ahmed@outlook.com>
2023-08-30 12:46:59 +0200

    Creating AdoptableAnimals.tsx file

---
+++ src/components/HomePage/AdoptableAnimals.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+function AdoptableAnimals() {
+  return (
+    <Wrapper>
+      
+    </Wrapper>
+  );
+}
+
+export const Wrapper = styled.div`
+  
+`;
+
+export default AdoptableAnimals;
