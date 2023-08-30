0ebc7884bf06273fe72bc4c8c4a573f7627590fa
Jomana ahmed <ojy.ahmed@outlook.com>
2023-08-30 18:42:06 +0200

    Creating Footer.tsx file

---
+++ src/components/HomePage/Footer.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+function Footer() {
+  return (
+    <Wrapper>
+      
+    </Wrapper>
+  );
+}
+
+export const Wrapper = styled.div`
+  
+`;
+
+export default Footer;
