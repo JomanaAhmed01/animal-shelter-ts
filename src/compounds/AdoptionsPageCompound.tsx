0f9dcc16bd03a65c2873ceb2345ce048cfa9dda7
Jomana ahmed <ojy.ahmed@outlook.com>
2023-09-03 18:30:05 +0200

    Creating AdoptionsPageCompound.tsx file

---
+++ src/compounds/AdoptionsPageCompound.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+function AdoptionsPageCompound() {
+  return (
+    <Wrapper>
+      
+    </Wrapper>
+  );
+}
+
+export const Wrapper = styled.div``;
+
+export default AdoptionsPageCompound;
