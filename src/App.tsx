7a4c1917645af937c8f12cca88f1f2ee2a58f992
Jomana ahmed <ojy.ahmed@outlook.com>
2023-09-03 18:32:30 +0200

    Adding AdoptionsPageCompound.tsx file to the App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
-import HomePageCompound from './compounds/HomePageCompound';
+// import HomePageCompound from './compounds/HomePageCompound';
+import AdoptionsPageCompound from './compounds/AdoptionsPageCompound';
 
 function App() {
   return (
     <div className="App">
-      <HomePageCompound />
+      {/* <HomePageCompound /> */}
+      <AdoptionsPageCompound />
     </div>
   );
 }
