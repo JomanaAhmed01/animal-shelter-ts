4fda52f829d78d714c7b3cd1ae084a58c030c414
Jomana ahmed <ojy.ahmed@outlook.com>
2023-09-03 18:36:49 +0200

    Creating Adoptions.tsx file

---
+++ src/components/AdoptionsPage/Adoptions.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+
+function Adoptions() {
+  return (
+    <Wrapper>
+       
+    </Wrapper>
+  );
+}
+
+export const Wrapper = styled.div`
+  
+`
+
+export default Adoptions;
