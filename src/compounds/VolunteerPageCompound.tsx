24dab8a1fd6a6b130d5f66fc03f31e905e588538
Jomana ahmed <ojy.ahmed@outlook.com>
2023-09-05 16:19:06 +0200

    Adding Navbar.tsx file ot the VolunteerPageCompound.tsx file

--- src/compounds/VolunteerPageCompound.tsx
+++ src/compounds/VolunteerPageCompound.tsx
@@ -1,9 +1,10 @@
 import styled from "styled-components";
+import Navbar from "../components/VolunteerPage/Navbar";
 
 function VolunteerPageCompound() {
   return (
     <Wrapper>
-      
+      <Navbar />
     </Wrapper>
   );
 }
