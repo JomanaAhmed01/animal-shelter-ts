480f5f3d02e34d083d4cc8b8b2e997cf7e81e8bf
Jomana ahmed <ojy.ahmed@outlook.com>
2023-09-05 16:15:31 +0200

    Creating VolunteerPageCompound.tsx file

---
+++ src/compounds/VolunteerPageCompound.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+function VolunteerPageCompound() {
+  return (
+    <Wrapper>
+      
+    </Wrapper>
+  );
+}
+
+export const Wrapper = styled.div``;
+
+export default VolunteerPageCompound;
