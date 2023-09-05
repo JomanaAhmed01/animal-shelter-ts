75035f6da39bf020fc5d3eda10919f40c568b744
Jomana ahmed <ojy.ahmed@outlook.com>
2023-09-05 16:21:24 +0200

    Adding Volunteer.tsx file ot the VolunteerPageCompound.tsx file

--- src/compounds/VolunteerPageCompound.tsx
+++ src/compounds/VolunteerPageCompound.tsx
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 import Navbar from "../components/VolunteerPage/Navbar";
+import Volunteer from "../components/VolunteerPage/Volunteer";
 
 function VolunteerPageCompound() {
   return (
     <Wrapper>
       <Navbar />
+      <Volunteer />
     </Wrapper>
   );
 }
